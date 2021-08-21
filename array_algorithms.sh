e9fc6c126c72eb0d4aad0ff36047dd5519135de4
Zimuzo Obiechina <zimobiechina@gmail.com>
2021-08-21 22:22:15 +0100

    Chore: Add comments

--- array_algorithms.sh
+++ array_algorithms.sh
@@ -3,16 +3,22 @@
 # Create a array of 20 elements of random odd and even numbers
 NUMBERS=(7 2 2 2 2 2 4 6 8 10 11 11 13 11 15 17 18 20 19 16)
 
+# Return number with highest and lowest value
 function smallest_largest() {
+  # Sort array in ascending order
   sorted_array=($(for NUMBER in ${NUMBERS[@]};do echo $NUMBER;done | sort -g))
 
+  # Highest and lowest values are at both ends of a sorted array
   echo "The number with the highest value is: ${sorted_array[-1]}"
   echo "The number with the lowest value is: ${sorted_array[0]}"
 }
 
+# Print sum of all even and odd numbers
 function print_sum() {
+  # Iterate through the list
     for NUMBER in ${NUMBERS[@]}
     do
+      # Find the even and odd numbers, and add them
       if [ $(expr $NUMBER % 2) == "0" ]
       then
         let EVEN_SUM+=$NUMBER
@@ -25,45 +31,49 @@ function print_sum() {
     echo "The sum of odd numbers is: $ODD_SUM"
 }
 
+# Return number with highest and lowest frequency
 function lowest_highest_freq() {
+  # Create an associative array - to serve the purpose of a hash map
   declare -A numbers_map
-COUNTER=1
-for NUMBER in ${NUMBERS[@]}
-do
-  if [ !numbers_map[$NUMBER] ]
-  then
-    let numbers_map[$NUMBER]+=1
-  else
-    let numbers_map[$NUMBER]=1
-  fi
-done
-
-for VALUE in ${numbers_map[@]}
-do
-  # echo $VALUE
-  if [ $VALUE -gt $COUNTER ]
-  then
-    COUNTER=$VALUE
-  fi
-done
-
-for KEY in ${!numbers_map[@]}
-do
-  if [ ${numbers_map[$KEY]} -eq $COUNTER ]
-  then
-    echo "The number with the highest frequency is: $KEY"
-  elif [ ${numbers_map[$KEY]} == "1" ]
-  then
-    declare -A lowest_freq
-    lowest_freq[$KEY]=$KEY
-  fi
-done
-
-echo "The number with the lowest frequency is: ${lowest_freq[@]}"
+  COUNTER=1
+  # Iterate through given array and populate the associative array
+  # keys: items of given array; values: number of times items occur in array
+  for NUMBER in ${NUMBERS[@]}
+  do
+    if [ !numbers_map[$NUMBER] ]
+    then
+      let numbers_map[$NUMBER]+=1
+    else
+      let numbers_map[$NUMBER]=1
+    fi
+  done
+  
+  # Find the highest frequency and update the counter
+  for VALUE in ${numbers_map[@]}
+  do
+    # echo $VALUE
+    if [ $VALUE -gt $COUNTER ]
+    then
+      COUNTER=$VALUE
+    fi
+  done
+  
+  # Match the counter value to the highest freq number(key)
+  for KEY in ${!numbers_map[@]}
+  do
+    if [ ${numbers_map[$KEY]} -eq $COUNTER ]
+    then
+      echo "The number with the highest frequency is: $KEY"
+    # Find numbers with the lowest freq and add them to an associative array
+    elif [ ${numbers_map[$KEY]} == "1" ]          
+    then
+      declare -A lowest_freq
+      lowest_freq[$KEY]=$KEY
+    fi
+  done
+  
+  echo "The number with the lowest frequency is: ${lowest_freq[@]}"
 }
-
-# echo "$COUNTER"
-# echo "${numbers_map[@]}"
 
 smallest_largest
 print_sum
