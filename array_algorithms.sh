cbe70ce5060505d15f1b93c66f5dda9ee46e8f30
Zimuzo Obiechina <zimobiechina@gmail.com>
2021-08-21 22:01:20 +0100

    Add function to find lowest and highest freq numbers

--- array_algorithms.sh
+++ array_algorithms.sh
@@ -1,29 +1,70 @@
-#!/bin/bash
+# #!/bin/bash
 
 # Create a array of 20 elements of random odd and even numbers
-numbers=(7 2 5 7 9 2 4 6 8 10 11 11 13 11 15 17 18 20 19 16)
+NUMBERS=(7 2 2 2 2 2 4 6 8 10 11 11 13 11 15 17 18 20 19 16)
 
 function smallest_largest() {
-  sorted_array=($(for number in ${numbers[@]};do echo $number;done | sort -g))
+  sorted_array=($(for NUMBER in ${NUMBERS[@]};do echo $NUMBER;done | sort -g))
 
   echo "The number with the highest value is: ${sorted_array[-1]}"
   echo "The number with the lowest value is: ${sorted_array[0]}"
 }
 
 function print_sum() {
-    for number in ${numbers[@]}
+    for NUMBER in ${NUMBERS[@]}
     do
-      if [ $(expr $number % 2) == "0" ]
+      if [ $(expr $NUMBER % 2) == "0" ]
       then
-        let even_sum+=$number
+        let EVEN_SUM+=$NUMBER
       else
-        let odd_sum+=$number
+        let ODD_SUM+=$NUMBER
       fi
     done
 
-    echo "The sum of even numbers is: $even_sum"
-    echo "The sum of odd numbers is: $odd_sum"
+    echo "The sum of even numbers is: $EVEN_SUM"
+    echo "The sum of odd numbers is: $ODD_SUM"
 }
+
+function lowest_highest_freq() {
+  declare -A numbers_map
+COUNTER=1
+for NUMBER in ${NUMBERS[@]}
+do
+  if [ !numbers_map[$NUMBER] ]
+  then
+    let numbers_map[$NUMBER]+=1
+  else
+    let numbers_map[$NUMBER]=1
+  fi
+done
+
+for VALUE in ${numbers_map[@]}
+do
+  # echo $VALUE
+  if [ $VALUE -gt $COUNTER ]
+  then
+    COUNTER=$VALUE
+  fi
+done
+
+for KEY in ${!numbers_map[@]}
+do
+  if [ ${numbers_map[$KEY]} -eq $COUNTER ]
+  then
+    echo "The number with the highest frequency is: $KEY"
+  elif [ ${numbers_map[$KEY]} == "1" ]
+  then
+    declare -A lowest_freq
+    lowest_freq[$KEY]=$KEY
+  fi
+done
+
+echo "The number with the lowest frequency is: ${lowest_freq[@]}"
+}
+
+# echo "$COUNTER"
+# echo "${numbers_map[@]}"
 
 smallest_largest
 print_sum
+lowest_highest_freq
