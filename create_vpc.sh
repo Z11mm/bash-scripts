1db254ca881de754c9a929e41bf3746c3a38aa88
Zimuzo Obiechina <zimobiechina@gmail.com>
2021-09-23 11:19:48 +0100

    vpc creation script
    First pass

--- create_vpc.sh
+++ create_vpc.sh
@@ -1,19 +1,39 @@
 #!/bin/bash
 
+NETWORK_NAME="vpc-sca1"
+FIREWALL_RULE_NAME=$NETWORK_NAME
+PUBLIC_SUBNET_NAME="public1"
+PUBLIC_SUBNET_REGION="us-west2"
+PUBLIC_SUBNET_IP_RANGE=10.0.0.0/24
+PRIVATE_SUBNET_NAME="private1"
+PRIVATE_SUBNET_REGION="us-west1"
+PRIVATE_SUBNET_IP_RANGE=172.60.0.0/24
+ROUTER_NAME="router1"
+NAT_NAME="nat1"
+
+
+echo "Creating vpc"
 # Create vpc
-gcloud compute networks create vpc-sca --subnet-mode=custom --bgp-routing-mode=regional --mtu=1460
+gcloud compute networks create $NETWORK_NAME --subnet-mode=custom --bgp-routing-mode=regional --mtu=1460
 
+echo "Creating firewall rules"
 # Create firewall rules
-gcloud compute firewall-rules create vpc-sca-allow-ssh --network vpc-sca --action allow --rules tcp:22,icmp --source-ranges 0.0.0.0/0
+gcloud compute firewall-rules create ${FIREWALL_RULE_NAME}-allow-ssh --network $NETWORK_NAME --action allow --rules tcp:22,icmp --source-ranges 0.0.0.0/0
 
+echo "Creating public subnet"
 # Create public subnet
-gcloud compute networks subnets create public --network=vpc-sca --range=10.120.0.0/24 --region=us-central1
+gcloud compute networks subnets create $PUBLIC_SUBNET_NAME --network=$NETWORK_NAME --range=$PUBLIC_SUBNET_IP_RANGE --region=$PUBLIC_SUBNET_REGION
 
+echo "Creating private subnet"
 # Create private subnet
-gcloud compute networks subnets create private --network=vpc-sca --range=172.31.0.0/24 --region=us-west1 --enable-private-ip-google-access
+gcloud compute networks subnets create $PRIVATE_SUBNET_NAME --network=$NETWORK_NAME --range=$PRIVATE_SUBNET_IP_RANGE --region=$PRIVATE_SUBNET_REGION --enable-private-ip-google-access
 
+echo "Creating router for private subnet"
 # Create router for private subnet
-gcloud compute routers create router --network=vpc-sca --region=us-west1
+gcloud compute routers create $ROUTER_NAME --network=$NETWORK_NAME --region=$PRIVATE_SUBNET_REGION
 
+echo "Creating NAT for private subnet"
 # Create NAT for private subnet
-gcloud compute routers nats create nat --router=router --router-region=us-west1 --auto-allocate-nat-external-ips --nat-custom-subnet-ip-ranges=private
+gcloud compute routers nats create $NAT_NAME --router=$ROUTER_NAME --router-region=$PRIVATE_SUBNET_REGION --auto-allocate-nat-external-ips --nat-custom-subnet-ip-ranges=$PRIVATE_SUBNET_NAME
+
+echo "vpc creation complete"
